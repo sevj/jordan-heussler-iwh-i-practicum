341bc1f48b59fcd00fd2962703b08d50c80b791f
Sevj <jheussler@adimeo.com>
2025-03-24 13:21:06 +0100

    Add updates form

--- index.js
+++ index.js
@@ -1,26 +1,35 @@
 const express = require('express');
 const axios = require('axios');
 const app = express();
+const pug = require('pug');
+const dotenv = require('dotenv');
+
+const formView = pug.compileFile('views/updates.pug');
+
+dotenv.config();
 
 app.set('view engine', 'pug');
 app.use(express.static(__dirname + '/public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+
 // * Please DO NOT INCLUDE the private app access token in your repo. Don't do this practicum in your normal account.
-const PRIVATE_APP_ACCESS = '';
+const PRIVATE_APP_ACCESS = process.env.API_KEY;
 
-// TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
+app.get('/', (req, res) => {
+    res.send('hello world')
+})
 
-// * Code for Route 1 goes here
+app.get('/update-cobj', (req, res) => {
+    res.send(formView({
+        'title': 'Update Custom Object Form | Integrating With HubSpot I Practicum'
+    }))
+})
 
-// TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
-
-// * Code for Route 2 goes here
-
-// TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
-
-// * Code for Route 3 goes here
+app.post('/update-cobj', (req, res) => {
+    console.log(req.body)
+})
 
 /** 
 * * This is sample code to give you a reference for how you should structure your calls. 
